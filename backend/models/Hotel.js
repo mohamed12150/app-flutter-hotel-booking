a900fbf8762768da2308f52b75cf123cd92ddd06
mohamed12150 <alkair621@gmail.com>
2025-12-01 17:03:25 +0000

    Simplify Hotel model schema

--- backend/models/Hotel.js
+++ backend/models/Hotel.js
@@ -1,47 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const hotelSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  location: {
-    type: String,
-    required: true
-  },
-  address: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String
-  },
-  rating: {
-    type: Number,
-    default: 0,
-    min: 0,
-    max: 5
-  },
-  images: [{
-    type: String
-  }],
-  amenities: [{
-    type: String
-  }],
-  pricePerNight: {
-    type: Number,
-    required: true
-  },
-  totalRooms: {
-    type: Number,
-    required: true
-  },
-  availableRooms: {
-    type: Number,
-    required: true
-  }
-}, {
-  timestamps: true
+  name: String,
+  city: String,
+  price: Number,
+  availableRooms: Number,
+  rating: Number
 });
 
-module.exports = mongoose.model('Hotel', hotelSchema);
+module.exports = mongoose.model("Hotel", hotelSchema);
